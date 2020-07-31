2984d0f50cac4916e4c7f456ea88168c0d621388
Vincent Brebion <vbrebion@sodius.com>
2020-07-31 16:57:03 +0200

    More firing patterns

--- src/main/kotlin/fire.kt
+++ src/main/kotlin/fire.kt
@@ -1,4 +1,5 @@
 import org.openrndr.math.Vector2
+import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -17,7 +18,7 @@ enum class Fire(val pattern: (Enemy, Double, Double, MutableList<Bullet>) -> Uni
     CIRCLE_PULSE({ e, _, deltaTime, bullets ->
         val delay = 1.0
         val nBullets = 10
-        val theta = 2.0 * 3.14159 / nBullets
+        val theta = 2.0 * PI / nBullets
 
         e.dataFire[0] += deltaTime
 
@@ -29,6 +30,25 @@ enum class Fire(val pattern: (Enemy, Double, Double, MutableList<Bullet>) -> Uni
                     Bullet(
                         e.pos + Vector2.UNIT_Y * e.scale,
                         Vector2(180.0 * cos(theta * it), 180.0 * sin(theta * it))
+                    )
+                )
+            }
+        }
+    }),
+    DEATH_SPIRAL_CIRCLE({ e, _, deltaTime, bullets ->
+        val delay = .2
+        val nBullets = 100
+        val theta = 2.0 * 3.14159 / nBullets
+        e.dataFire[0] += deltaTime
+        if (e.dataFire[0] >= delay) {
+            e.dataFire[0] -= delay
+            e.dataFire[1] += .1
+
+            (0 until nBullets).forEach {
+                bullets.add(
+                    Bullet(
+                        e.pos + Vector2.UNIT_Y * e.scale,
+                        Vector2(180.0f * cos(theta * it + e.dataFire[1]), 180.0f * sin(theta * it + e.dataFire[1]))
                     )
                 )
             }
